e32a79e2cfe97449813b58a05885ae3ea5f13dd8
Oren Magen <omagen@yotpo.com>
2015-11-18 15:55:36 +0200

    added: bottom line to dedicated page

--- Yotpo/Yotpo/Block/Config.php
+++ Yotpo/Yotpo/Block/Config.php
@@ -19,37 +19,42 @@ class Config
 
     public function getAppKey()
     {   
-        return $this->_scopeConfig->getValue(self::YOTPO_APP_KEY, ScopeInterface::SCOPE_STORE, 1);     
+        return $this->_scopeConfig->getValue(self::YOTPO_APP_KEY, ScopeInterface::SCOPE_STORES, 1);     
     }
 
     public function getSecret()
     {        
-        return $this->_scopeConfig->getValue(self::YOTPO_SECRET, ScopeInterface::SCOPE_STORE, 1);
+        return $this->_scopeConfig->getValue(self::YOTPO_SECRET, ScopeInterface::SCOPE_STORES, 1);
     }
 
     public function isWidgetEnabled()
     {        
-        return (bool)$this->_scopeConfig->getValue(self::YOTPO_WIDGET_ENABLED, ScopeInterface::SCOPE_STORE, 1);
+        return (bool)$this->_scopeConfig->getValue(self::YOTPO_WIDGET_ENABLED, ScopeInterface::SCOPE_STORES, 1);
     } 
 
     public function isBottomlineEnabled()
     {        
-        return (bool)$this->_scopeConfig->getValue(self::YOTPO_BOTTOMLINE_ENABLED, ScopeInterface::SCOPE_STORE, 1);
+        return (bool)$this->_scopeConfig->getValue(self::YOTPO_BOTTOMLINE_ENABLED, ScopeInterface::SCOPE_STORES, 1);
     } 
 
     public function setAppKey($val)
     {   //TODO last parameter should probably be store id
-        $this->_resourceConfig->saveConfig(self::YOTPO_APP_KEY, $val, ScopeInterface::SCOPE_STORE, 1);
+        $this->_resourceConfig->saveConfig(self::YOTPO_APP_KEY, $val, ScopeInterface::SCOPE_STORES, 1);
     }
 
     public function setSecret($val)
     {        
-        $this->_resourceConfig->saveConfig(self::YOTPO_SECRET, $val, ScopeInterface::SCOPE_STORE, 1);
+        $this->_resourceConfig->saveConfig(self::YOTPO_SECRET, $val, ScopeInterface::SCOPE_STORES, 1);
     }
 
     public function setWidgetEnabled($val)
     {        
-        $this->_resourceConfig->saveConfig(self::YOTPO_WIDGET_ENABLED, $val, ScopeInterface::SCOPE_STORE, 1);
+        $this->_resourceConfig->saveConfig(self::YOTPO_WIDGET_ENABLED, $val, ScopeInterface::SCOPE_STORES, 1);
+    }    
+
+    public function setBottomlineEnabled($val)
+    {        
+        $this->_resourceConfig->saveConfig(self::YOTPO_BOTTOMLINE_ENABLED, $val, ScopeInterface::SCOPE_STORES, 1);
     } 
     
     public function isAppKeyAndSecretSet()
--- Yotpo/Yotpo/Controller/Adminhtml/Settings/Save.php
+++ Yotpo/Yotpo/Controller/Adminhtml/Settings/Save.php
@@ -21,9 +21,11 @@ class Save extends \Magento\Backend\App\Action
                 $app_key = $this->getRequest()->getParam('app_key');
                 $secret = $this->getRequest()->getParam('secret');
                 $show_widget = $this->getRequest()->getParam('show_widget');
+                $show_buttomline = $this->getRequest()->getParam('show_buttomline');
                 $this->_config->setAppKey($app_key);
                 $this->_config->setSecret($secret);
                 $this->_config->setWidgetEnabled($show_widget);
+                $this->_config->setBottomlineEnabled($show_buttomline);
                 
                 $this->messageManager->addSuccess(__('Yotpo Settings Saved'));  
             } catch (\Exception $e) {
