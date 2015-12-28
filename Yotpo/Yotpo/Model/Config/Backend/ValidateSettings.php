2202b7ae3c6101fd3e95007515f26d64bc7122a3
Omry Zobel <ozobel@yotpo.com>
2015-12-28 18:14:51 +0200

    fix(cache_clean): after settings saved

---
+++ Yotpo/Yotpo/Model/Config/Backend/ValidateSettings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yotpo\Yotpo\Model\Config\Backend;
+use Magento\Store\Model\ScopeInterface;
+
+class ValidateSettings extends \Magento\Framework\App\Config\Value
+{
+
+	const YOTPO_CACHE = 'yotpo/settings/cache';
+
+	public function __construct(
+	    \Magento\Framework\Model\Context $context,
+	    \Magento\Framework\Registry $registry,
+	    \Magento\Framework\App\Config\ScopeConfigInterface $config,
+	    \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+	    \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+	    \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+      	\Psr\Log\LoggerInterface $logger,
+	    array $data = []
+	) {
+	    $this->_logger = $logger;
+	    $this->context = $context;
+	    parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+	}
+
+	public function afterSave()
+    {
+		if ($this->isValueChanged()) {
+			$this->context->getCacheManager()->clean();
+		} 
+        return;
+    }
+}
