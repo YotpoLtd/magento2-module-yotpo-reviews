10c4dddc332c4a13443eee0ef7217a61e30a5d0c
pini-girit <pini@girit.biz>
2019-05-06 11:23:36 +0300

    v2.8.0: small fixes on configuration texts + menu links

--- Controller/Adminhtml/External/Analytics.php
+++ Controller/Adminhtml/External/Analytics.php
@@ -2,54 +2,13 @@
 
 namespace Yotpo\Yotpo\Controller\Adminhtml\External;
 
-use Magento\Backend\App\Action\Context;
-use Magento\Store\Model\ScopeInterface;
-use Yotpo\Yotpo\Helper\Data as YotpoHelper;
+use Magento\Framework\Controller\ResultFactory;
 
 class Analytics extends \Magento\Backend\App\Action
 {
-    protected $_scope;
-    protected $_scopeId;
-    protected $_isEnabled;
-    protected $_appKey;
-    protected $_isAppKeyAndSecretSet;
-
-    /**
-     * @var YotpoHelper
-     */
-    protected $_yotpoHelper;
-
-    /**
-     * Constructor
-     *
-     * @param Context $context
-     * @param YotpoHelper $yotpoHelper
-     */
-    public function __construct(
-        Context $context,
-        YotpoHelper $yotpoHelper
-    ) {
-        parent::__construct($context);
-        $this->_yotpoHelper = $yotpoHelper;
-        $this->_initiaize();
-    }
-
-    protected function _initiaize()
-    {
-        if (($storeId = $this->getRequest()->getParam("store", 0))) {
-            $this->_scope = ScopeInterface::SCOPE_STORE;
-            $this->_scopeId = $storeId;
-        } elseif (($websiteId = $this->getRequest()->getParam("website", 0))) {
-            $this->_scope = ScopeInterface::SCOPE_WEBSITE;
-            $this->_scopeId = $websiteId;
-        }
-        $this->_isEnabled = $this->_yotpoHelper->isEnabled($this->_scopeId, $this->_scope);
-        $this->_appKey = $this->_yotpoHelper->getAppKey($this->_scopeId, $this->_scope);
-        $this->_isAppKeyAndSecretSet = $this->_yotpoHelper->isAppKeyAndSecretSet($this->_scopeId, $this->_scope);
-    }
-
     public function execute()
     {
-        return $this->getResponse()->setContent("Analytics");
+        return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)
+            ->setUrl('https://yap.yotpo.com/#/tools/conversions_dashboard/engagement');
     }
 }
